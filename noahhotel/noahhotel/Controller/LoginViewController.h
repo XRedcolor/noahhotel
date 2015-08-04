a9bdfeb53043d2d5bd80fc4c139ac0c1b2baf10a
xredcolor <xredcolor@163.com>
2015-08-04 10:59:27 +0800

    New LoginViewController

---
+++ noahhotel/noahhotel/Controller/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  noahhotel
+//
+//  Created by XRedcolor on 15/8/4.
+//  Copyright (c) 2015年 mirror. All rights reserved.
+//
+
+#import "XRBaseViewController.h"
+
+@interface LoginViewController : XRBaseViewController
+
+@end
