c6bde6fb922f627496ac1a9a02bc08bf41d0d32e
xredcolor <xredcolor@163.com>
2015-08-04 10:54:54 +0800

    New BaseViewController

---
+++ noahhotel/noahhotel/BaseController/XRBaseViewController.h
@@ -0,0 +1,13 @@
+//
+//  XRBaseViewController.h
+//  noahhotel
+//
+//  Created by XRedcolor on 15/8/4.
+//  Copyright (c) 2015年 mirror. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XRBaseViewController : UIViewController
+
+@end
