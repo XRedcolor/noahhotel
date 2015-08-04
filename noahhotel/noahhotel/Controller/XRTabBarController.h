8fd48218cae41ef3515976ed87c6ea7edb9a9d33
xredcolor <xredcolor@163.com>
2015-08-04 10:22:28 +0800

    New TabBarController

---
+++ noahhotel/noahhotel/Controller/XRTabBarController.h
@@ -0,0 +1,13 @@
+//
+//  XRTabBarController.h
+//  noahhotel
+//
+//  Created by XRedcolor on 15/8/4.
+//  Copyright (c) 2015年 mirror. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XRTabBarController : UITabBarController
+
+@end
